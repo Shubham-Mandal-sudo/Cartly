f92231bb3c6052848e004aa370f8b04ec2cdbfe6
Shubham <shubhammandal205@gmail.com>
2026-07-07 18:36:32 +0530

    Added security fix

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@b_rcb8!z@6ool@y3*rnb1$(1x7oaomag8d)-7z)7_&h&n97q%'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
